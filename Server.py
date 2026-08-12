c39bc19b3b8ffe574c97f8a1d8d280a5950a8260
maragacyrus-gif <maragacyrus@gmail.com>
2026-08-13 02:00:14 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,18 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="Custom Deriv Trading Platform")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server.py:app", host="127.0.0.1", port=8000, reload=True)
